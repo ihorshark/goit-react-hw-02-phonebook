01370a2e732c921e2054f41e3107a67782cf69b1
ihorshark <igor.sharkadi.93@gmail.com>
2022-06-23 21:27:19 +0300

    Demo 2

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import s from './ContactList.module.css';
+
 const ContactList = ({ contacts, deleteContact }) => (
-  <ul>
+  <ul className={s.list}>
     {contacts.map(contact => {
       return (
-        <li key={contact.id}>
+        <li className={s.item} key={contact.id}>
           {contact.name} {contact.number}
           <button type="button" onClick={() => deleteContact(contact.id)}>
             Delete user
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Filter.module.css';
 
 const Filter = ({ value, onChange }) => (
-  <label>
+  <label className={s.label}>
     Find contacts by name
-    <input type="text" value={value} onChange={onChange} />
+    <input className={s.input} type="text" value={value} onChange={onChange} />
   </label>
 );
 export default Filter;
